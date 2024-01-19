a2df39a0750c4e5a070aba80dc85d7488c1d074d
avadombi <vp.adombi@gmail.com>
2024-01-19 11:15:11 -0500

    Update data processing code as well as the data itself

--- 001-Exercises/001-ProcessData/Scripts/utilisFunctions.py
+++ 001-Exercises/001-ProcessData/Scripts/utilisFunctions.py
@@ -8,6 +8,12 @@ def get_countries():
     path = f"{DIR_PATH}/countries.xlsx"
     countries = pd.read_excel(path, index_col=0)
     countries = countries.rename(columns={'country_code_2': 'country_code'})
+
+    # remove duplicates
+    countries = countries.drop_duplicates(subset='country_name')
+
+    # sort by asc order
+    countries = countries.sort_values(by='country_name')
     return countries
 
 
@@ -22,25 +28,28 @@ def get_currencies():
     path = f"{DIR_PATH}/currencies.xlsx"
     currencies = pd.read_excel(path, index_col=0)
     currencies = currencies.rename(columns={'country_code_2': 'country_code'})
-    return currencies
 
+    # remove duplicates (currency)
+    currencies_no_duplicates = currencies.drop_duplicates(subset='currency_code')
 
-def get_processed_currencies():
-    # get currencies data
-    currencies_data = get_currencies()
-
-    # remove duplicates
-    currencies_no_duplicates = currencies_data.drop_duplicates('currency_code')
+    # remove duplicates (country_code)
+    currencies_no_duplicates = currencies_no_duplicates.drop_duplicates(subset='country_code')
 
     # sort by asc order
     currencies_no_duplicates = currencies_no_duplicates.sort_values(by='currency_name')
 
     # add index column
     currencies_no_duplicates['currency_id'] = currencies_no_duplicates.index
+    return currencies_no_duplicates
+
+
+def get_processed_currencies():
+    # get currencies data
+    currencies_data = get_currencies()
 
     # reorganize
     columns = ['currency_id', 'currency_name', 'currency_code']
-    currencies_no_duplicates = currencies_no_duplicates[columns]
+    currencies_no_duplicates = currencies_data[columns]
 
     # sort
     currencies_no_duplicates = currencies_no_duplicates.sort_values(by='currency_id')
@@ -57,7 +66,7 @@ def get_process_countries():
     # merge on country_code_2
     merged_data = pd.merge(
         countries_data, currencies,
-        on='country_code', how='left'
+        on='country_code', how='inner'
     )
 
     # get the columns of interest
